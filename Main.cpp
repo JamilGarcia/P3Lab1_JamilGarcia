0c411af070ab98d9eff1644f459001929c83a4ae
JamilGarcia <you@example.com>
2020-07-24 15:13:20 -0600

    Terminado

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std; 
 int main(void)
 {
@@ -9,21 +10,31 @@ int main(void)
         cout << "1) Ejercicio 1\n";
         cout << "2) Ejercicio 2\n";
         cout << "0) Salir \n"; 
-        cout << "Ingrese la opcion: ss ";
+        cout << "Ingrese la opcion:  ";
         cin >> opcion;
+        cout << "\n"; 
         switch (opcion) {
         case 1: 
         {
             int valorX;
-            cout << "Ingrese el valor de x: ";
-             
-            cout << "entra 1";
+            cout << "Ingrese el valor de x: \n";
+            cin >> valorX; 
+            double funcionX = (1)/(1+ exp(-valorX)); 
+            cout << "Respuesta= " << funcionX << "\n"; 
             break; 
         }
 
         case 2: 
         {
-            cout << "entra 2";
+            int limitcont; 
+            int solucion = -1; 
+            cout << "Ingrese el valor de n: "; 
+            cin >> limitcont; 
+            for (int i = 1; i <= limitcont ; i++)
+            {
+               solucion = solucion + ((2*i)-1) * ((2*i)+1); 
+            }
+            cout << "Respuesta= " << solucion << "\n"; 
             break;
         }
         case 0: 
@@ -36,5 +47,5 @@ int main(void)
         }
         }
     } while (resp == 0); 
-    
+    cout << "Gracias por usar el programa <3 (Nota: Esta es como mi marca de programador)";
 }
